5bd191b407228623d3ab801dcc391ca534e6c89e
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2022-12-01 12:42:59 +0530

    Update class-movie.js

--- class-movie.js
+++ class-movie.js
@@ -2,18 +2,31 @@ class movie{
     constructor(title,studio,rating){
         this.title=title;
         this.studio=studio;
-        this.rating=rating;
+        if(rating==undefined){
+            this.rating="PG"
+        }else {this.rating=rating;}
         
     }
-    getPG(){
-        if(this.rating==undefined)
-        {return this.rating="PG"}
-        else{return this.rating}
+    getPG(Array){
+       let result=[];
+       for(let i=0;i<Array.length;i++){
+        if(arr[i].rating=="PG"){
+            result.push(arr[i]);
+        }
+       } 
+       return result
     }
     
 }
 var movie1= new movie("Casino Royale","Eon Productions","PG13");
+var movie2= new movie("Iron Man","Marvel Productions","PG");
+var movie3= new movie("Harry Potter","Earner Brother Productions","PG13");
+var movie4= new movie("Pirates of Carribean","Apple Productions","PG");
+var movie5= new movie("Avengers-End Game","Marvel Productions","PG13");
 
 console.log(movie1.title);
 console.log(movie1.studio);
-console.log(movie1.getPG());
+
+let arr=[movie1,movie2,movie3,movie3,movie4,movie5]
+
+console.log(movie1.getPG(arr))
