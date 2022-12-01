73b41a634fa4fd02c8388e16f2fa8a00e4f640b5
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2022-12-01 13:31:56 +0530

    Update class-uber price.js

--- class-uber price.js
+++ class-uber price.js
@@ -1,16 +1,31 @@
 class tariff{
     
-    constructor(distance,pricePerKilometer){
-          this.pricePerKilometer=pricePerKilometer;
+    constructor(distance,VehicleType){
           this.distance=distance;
+          this.VehicleType=VehicleType;
           
       }
        get fare (){
-          return (`Rs:${(this.pricePerKilometer*this.distance).toFixed(2)}`);
+         if(this.VehicleType=="Hatchback"){
+           let basePrice=500;
+           let pricePerKM=14;
+          return (`Uber Fare for ${this.distance}Km in Hatchback is Rs: ${(basePrice+this.distance*pricePerKM).toFixed(2)}`);}
+          if(this.VehicleType=="Sedan"){
+            let basePrice=700;
+           let pricePerKM=16;
+          return (`Uber Fare for ${this.distance}Km in Sedan is Rs: ${(basePrice+this.distance*pricePerKM).toFixed(2)}`);}
+          if(this.VehicleType=="SUV"){
+            let basePrice=1000;
+           let pricePerKM=19;
+          return (`Uber Fare for ${this.distance}Km in SUV is Rs: ${(basePrice+this.distance*pricePerKM).toFixed(2)}`);}
       }
       
       
   }
-  var uber= new tariff(120,19);
+  var suv= new tariff(120,"SUV");
+  var hatchback= new tariff(100,"Hatchback");
+  var sedan= new tariff(100,"Sedan");
   
-  console.log(uber.fare);
+  console.log(suv.fare);
+  console.log(hatchback.fare);
+  console.log(sedan.fare);
